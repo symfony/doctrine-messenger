200084fc1c8c5330a01bd43377a99cd72599df60
Sergii Dolgushev <dolgushev.serhey@gmail.com>
2025-10-31 11:48:31 +0000

    [Messenger] Fix unreachable catch

--- Transport/Connection.php
+++ Transport/Connection.php
@@ -166,12 +166,12 @@ class Connection implements ResetInterface
         if ($this->driverConnection->getDatabasePlatform() instanceof MySQLPlatform) {
             try {
                 $this->driverConnection->delete($this->configuration['table_name'], ['delivered_at' => '9999-12-31 23:59:59']);
-            } catch (DriverException $e) {
-                // Ignore the exception
             } catch (TableNotFoundException $e) {
                 if ($this->autoSetup) {
                     $this->setup();
                 }
+            } catch (DriverException $e) {
+                // Ignore the exception
             }
         }
 
