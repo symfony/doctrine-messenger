0ae14db80d946b2e4fac5b2ec5e8eca8b30feaf4
Christian Flothmann <christian.flothmann@open.de>
2026-02-16 13:42:33 +0100

    remove usages of the deprecated any() invoked count expectation

--- Tests/Transport/PostgreSqlConnectionTest.php
+++ Tests/Transport/PostgreSqlConnectionTest.php
@@ -55,12 +55,10 @@ class PostgreSqlConnectionTest extends TestCase
         $driverConnection->method('executeStatement')->willReturn(1);
 
         $driverConnection
-            ->expects(self::any())
             ->method('getDatabasePlatform')
             ->willReturn(new PostgreSQLPlatform());
 
         $driverConnection
-            ->expects(self::any())
             ->method('createQueryBuilder')
             ->willReturn(new QueryBuilder($driverConnection));
 
@@ -95,7 +93,6 @@ class PostgreSqlConnectionTest extends TestCase
                 ->willReturn($wrappedConnection);
 
             $driverConnection
-                ->expects(self::any())
                 ->method('executeQuery')
                 ->willReturn(new ArrayStatement([]));
         } else {
@@ -109,7 +106,6 @@ class PostgreSqlConnectionTest extends TestCase
             $driverResult->method('fetchAssociative')
                 ->willReturn(false);
             $driverConnection
-                ->expects(self::any())
                 ->method('executeQuery')
                 ->willReturn(new Result($driverResult, $driverConnection));
         }
